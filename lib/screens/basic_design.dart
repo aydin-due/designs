6569739d26d69a2a8c895cd6c24bc422bfc3619b
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-07-27 18:47:19 -0700

    basic design - title widget

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -6,9 +6,37 @@ class BasicDesignScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Image(image: AssetImage('assets/landscape.jpg')),
+    return Scaffold(
+      body: Column(
+        children: const [
+          Image(image: AssetImage('assets/landscape.jpg')),
+          Title()
+        ]),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Row(
+        children: [
+           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text('Oeschinen Lake Campground', style: TextStyle(fontWeight: FontWeight.bold),),
+              Text('Kandersteg, Switzerland', style: TextStyle(color: Colors.black45),)
+            ]),
+            Expanded(child: Container()),
+           const Icon(Icons.star, color: Colors.red,),
+           const Text('41')
+        ],
       ),
     );
   }
