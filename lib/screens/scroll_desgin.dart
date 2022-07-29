0de32c5369b356301c1a145370e252e6d0818ee4
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-07-29 06:20:12 -0700

    scroll design - bg & stack

---
+++ lib/screens/scroll_desgin.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ScrollDesignScreen extends StatelessWidget {
+  const ScrollDesignScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack( 
+        children: const [
+         Background(),
+          MainContent()  
+        ],
+    ));
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff55BED7),
+      height: double.infinity,
+      alignment: Alignment.topCenter,
+      child: const Image(image: AssetImage('assets/scroll.png')));
+  }
+}
+
+class MainContent extends StatelessWidget {
+  const MainContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text('aaaaa'),
+        Text('aaaaaaaaa')
+      ],
+    );
+  }
+}
